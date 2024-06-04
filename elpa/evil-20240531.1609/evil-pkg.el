9a136c75c0b6613084b9bf46a0a70bbd2ac81572
Rivado Edah <redah@umd.edu>
2024-06-04 13:51:34 -0400

    first

---
+++ elpa/evil-20240531.1609/evil-pkg.el
@@ -0,0 +1,12 @@
+(define-package "evil" "20240531.1609" "Extensible vi layer"
+  '((emacs "24.1")
+    (goto-chg "1.6")
+    (cl-lib "0.5"))
+  :commit "9fada7828a1afadbec1f84675ce3c8e9729e1cdc" :maintainer
+  '("Tom Dalziel" . "tom.dalziel@gmail.com")
+  :keywords
+  '("emulations")
+  :url "https://github.com/emacs-evil/evil")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
